6e9dc6638b4168c3c9bbfdcd814c305f909afb49
André Ramos do Vale <andre_ramosdovale@outlook.com>
2024-09-23 11:50:24 -0300

    fix: remove code smells

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -83,27 +83,27 @@ const colorThemes = {
         n_6: "#9E9E9E",
         n_7: "#757575",
       },
-      primary: "#FFFF00", // Amarelo brilhante para contraste
+      primary: "#FFFF00",
       secondary: "#FFFFFF",
-      info: "#00FFFF", // Ciano para contraste
+      info: "#00FFFF",
       shade: {
-        s_1: "#FFD700", // Ouro
-        s_2: "#FF8C00", // Laranja escuro
-        s_3: "#FF4500", // Laranja avermelhado
-        s_4: "#FF0000", // Vermelho
-        s_5: "#8B0000", // Vermelho escuro
+        s_1: "#FFD700",
+        s_2: "#FF8C00",
+        s_3: "#FF4500",
+        s_4: "#FF0000",
+        s_5: "#8B0000",
       },
       tint: {
-        t_1: "#FFFFE0", // Amarelo claro
-        t_2: "#FFFACD", // Limão claro
-        t_3: "#FAFAD2", // Dourado claro
-        t_4: "#FFEFD5", // PapayaWhip
-        t_5: "#FFF5EE", // SeaShell
+        t_1: "#FFFFE0",
+        t_2: "#FFFACD",
+        t_3: "#FAFAD2",
+        t_4: "#FFEFD5",
+        t_5: "#FFF5EE",
       },
       action: {
-        warning: "#FFD700", // Ouro
-        error: "#FF0000", // Vermelho
-        success: "#00FF00", // Verde brilhante
+        warning: "#FFD700",
+        error: "#FF0000",
+        success: "#00FF00",
       },
     },
   },
@@ -119,27 +119,27 @@ const colorThemes = {
         n_6: "#9E9E9E",
         n_7: "#757575",
       },
-      primary: "#FF4081", // Rosa brilhante para contraste
+      primary: "#FF4081",
       secondary: "#000000",
-      info: "#2979FF", // Azul brilhante
+      info: "#2979FF",
       shade: {
-        s_1: "#FF80AB", // Rosa claro
-        s_2: "#FF4081", // Rosa brilhante
-        s_3: "#F50057", // Rosa escuro
-        s_4: "#C51162", // Rosa mais escuro
-        s_5: "#880E4F", // Vinho
+        s_1: "#FF80AB",
+        s_2: "#FF4081",
+        s_3: "#F50057",
+        s_4: "#C51162",
+        s_5: "#880E4F",
       },
       tint: {
-        t_1: "#FFCDD2", // Rosa claro
-        t_2: "#F8BBD0", // Rosa suave
-        t_3: "#E1BEE7", // Lilás claro
-        t_4: "#D1C4E9", // Lilás suave
-        t_5: "#C5CAE9", // Azul claro
+        t_1: "#FFCDD2",
+        t_2: "#F8BBD0",
+        t_3: "#E1BEE7",
+        t_4: "#D1C4E9",
+        t_5: "#C5CAE9",
       },
       action: {
-        warning: "#FFAB00", // Laranja brilhante
-        error: "#D50000", // Vermelho escuro
-        success: "#00C853", // Verde brilhante
+        warning: "#FFAB00",
+        error: "#D50000",
+        success: "#00C853",
       },
     },
   },
